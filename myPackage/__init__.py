1acc3b735d98fb144fef9cdde69345fd65ba7f07
namnamu <skadlsrud8@naver.com>
2023-07-18 17:19:10 +0900

    add ttest_auto

--- myPackage/__init__.py
+++ myPackage/__init__.py
@@ -1,5 +1,6 @@
 txt='''
 dfhelper:
+    편의성을 위해 직접 만든 외부함수모듈입니다.
     to_number_value: (dataframe[,bool])
         데이터 프레임을 컬럼기준으로 숫자화한다. 기존컬럼이름+"_numbering"되어서 반환된다.
         bool은 기본적으로 False이며, 새로운 데이터 프레임을 반환한다.
@@ -12,5 +13,15 @@ dfhelper:
     visualization: (dataframe,str,str[,str])
         시각화하고 싶은 데이터 프레임과 컬럼 이름을 적는다. 
         histogram을 이용하고 싶다면 두번째 문자열에 아무 문자열이나 적는다.   
+    
+    def many_case_ttest(df_col)
+        리스트로 들어오는 모든 경우의 수를 리그전 형식으로 구한다.
+        df_col: 시리즈
+
+    ttest_auto:(user_df,col,value)
+        보페로니 교정방식의 다변량 T검정
+        user_df: 입력할 데이터프레임 이름
+        col: x축 컬럼 이름 (다변량그룹명)
+        value: y축 vlaue이름 (col기준으로 분류된 이름)
 '''
 print(txt)
--- myPackage/dfhelper.py
+++ myPackage/dfhelper.py
@@ -1,3 +1,6 @@
+from tokenize import cookie_re
+
+
 def to_number_value(df,add=False):
     import pandas as pd
     new_df=pd.DataFrame()
@@ -28,7 +31,6 @@ def normal_check(df):
         
 def visualization(df,x_name,y_name,shape='none'):
     import seaborn as sns
-    import plotly.express as px
     import matplotlib.pyplot as plt
     plt.rc('font',family='Malgun Gothic')
     import matplotlib as mpl
@@ -36,10 +38,11 @@ def visualization(df,x_name,y_name,shape='none'):
     x=df[x_name]
     y=df[y_name]
     if shape=="bar":
-        sns.barplot(df,x=x,y=y)
+        sns.barplot(df,x=x,y=y,color='#FC7F77')
     elif shape=="line":
         sns.lineplot(df,x=x,y=y)
-    elif shape=="pie":
+    elif shape=="pie": # 컬럼에 오류가 있으면 에러를 출력하지 않고 빈 화면만 출력한다.
+        import plotly.express as px
         fig=px.pie(df,values=y_name,names=x_name)
         fig.show()
     elif shape=="histo":
@@ -53,3 +56,30 @@ def visualization(df,x_name,y_name,shape='none'):
         sns.boxenplot(df,x=x_name,y=y_name)
     else:
         print("bar, line, pie, histo, stem, scatter, box 중 입력")
+
+
+def many_case_ttest(df_col):
+    li=list(df_col.unique())
+    n=[]
+    while len(li)>0:
+        last=li.pop()
+        for l in li:
+            n.append(((l),(last)))
+    return n
+
+def ttest_auto(user_df,col,value):
+    #pip install statannot
+    from statannot import add_stat_annotation
+    import seaborn as sns
+    f1=sns.boxplot(data=user_df, x=col,y=value)
+    f2=sns.stripplot(data=user_df, x=col,y=value,color='k',alpha=.5) #겹쳐서 그린듯
+    add_stat_annotation(f1, 
+                        data=user_df, 
+                        x = col, 
+                        y = value,
+                        box_pairs=many_case_ttest(user_df[col]), #어떤것들이 박스인지 박스 이름을 적어준다.
+                        test='t-test_ind', #어떤 test를 할 것인가. 이름일부러 잘못적으면 이것들이 있다고 알려준다. 안외워도 된다.
+                        text_format='star', # p-value가 몇인지 마크를 별로 나타내는 것. 에러나면 뭐 적으라고 알려줌.
+                        loc='outside', # 두개 p-value나타내는걸 어디다가 적을 것인가
+                        verbose=2 # 0이면 ttest에 대해 아무 말도 안한다. 과 1이면 일부만 말한다. 2이면 전부 말한다.
+                        )
