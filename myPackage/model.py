ab842c496b8d8eeb284de55722417b14016a13d0
namnamu <skadlsrud8@naver.com>
2023-08-30 11:41:10 +0900

    variety model

--- myPackage/model.py
+++ myPackage/model.py
@@ -1,42 +1,41 @@
-'''
- 결정트리
-질문을 계속해서 분류할때 사용
-(과적합이 일어나기 쉬움)
-'''
-def Decison_Tree(X,Y):
-    from sklearn.tree import DecisionTreeClassifier
-    from sklearn.datasets import load_iris
-    from sklearn.model_selection import train_test_split
+# 모델생성(비호출용)
+def create_model(con):
+    '''
+    결정트리
+    질문을 계속해서 분류할때 사용
+    (과적합이 일어나기 쉬움)
+    '''
+    if con=='decisionTree':
+        from sklearn.tree import DecisionTreeClassifier
+        dt_clf = DecisionTreeClassifier(random_state=156)
+        return dt_clf
+    else:
+        from sklearn.ensemble import RandomForestClassifier
+        rf_clf = RandomForestClassifier(n_estimators=100, random_state=0, max_depth=8)
+        return rf_clf
 
-    # DecisionTree Classifier 생성
-    dt_clf = DecisionTreeClassifier(random_state=156)
-
+# 머신러닝 모델(호출용)
+def model(X,Y,con='decisionTree'):
     # 데이터 셋 분리
-    X_train , X_test , y_train , y_test = train_test_split(X, Y, test_size=0.2,  random_state=11)
-
-    # DecisionTreeClassifer 학습.
-    dt_clf.fit(X_train , y_train)
-
-
-    # 성능지표
+    from sklearn.model_selection import train_test_split
+    X_train , X_test , y_train , y_test = train_test_split(X, Y, test_size=0.2)# random_state=11
+    
+    # 생성
+    model=create_model(con)
+    # 학습
+    model.fit(X_train , y_train)
+    # 평가
     import sklearn.metrics as mt
 
-    y_pred = dt_clf.predict(X_test) # 예측값
+    y_pred = model.predict(X_test) # 예측값
     accuracy = mt.accuracy_score(y_test, y_pred) #정확도
     precision = mt.precision_score(y_test, y_pred)# 정밀도
     recall = mt.recall_score(y_test, y_pred)# 재현율
     auc = mt.roc_auc_score(y_test, y_pred) #auc너비. 보고할때는 항상 auc로
     matrix = mt.confusion_matrix(y_test, y_pred) # 오차행렬
 
-    # 사용법
-    # print("Decision Tree 정확도: {:.4f}".format(accuracy))
-    # print("Decision Tree 정밀도: {:.4f}".format(precision))
-    # print("Decision Tree 재현율: {:.4f}".format(recall))
-    # print("Decision Tree AUC: {:.4f}".format(auc))
-    # print('Decision Tree Confusion Matrix:','\n', matrix)
     return {"predict":y_pred,"accuracy":accuracy,"precision":precision,"recall":recall,"auc":auc,"matrix":matrix,
-            'y_test':y_test,'model':dt_clf,'X_test':X_test, "X_train": X_train , "y_train":y_train}
-
+            'y_test':y_test,'model':model,'X_test':X_test, "X_train": X_train , "y_train":y_train}
 
 
 """
@@ -65,3 +64,11 @@ def RoC(model,matrix,X_test,y_test):
     plt.ylabel('재현률(Recall)')
     plt.title('Receiver operating characteristic example')
     plt.show()
+
+"""
+분류모델에서 가장 영향을 많이 미친 특성
+"""
+def best_feature(model,X_train):
+    ftr_importances_values = model.feature_importances_
+    ftr_importances = pd.Series(ftr_importances_values,index=X_train.columns)
+    return ftr_importances.sort_values(ascending=False)[:20]
